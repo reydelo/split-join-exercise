2bf518e7317e55371a8dbd312616ee2c5577306b
Reyna DeLoge <reyna.deloge@gmail.com>
2015-02-20 10:09:13 -0700

    complete exercise

--- split_join_warmup.rb
+++ split_join_warmup.rb
@@ -10,31 +10,48 @@ world_cup_news
 
 # enter your solutions inside the methods
 def goal(goal_message)
-  goal_message + goal_message
+  # goal_message + goal_message
+  goal_message * 2
+  # "#{goal_message}#{goal_message}"
 end
 
 def generate_an_array_of_teams(team_list)
-  array = team_list.split(", ")
+  team_list.split(", ")
 end
 
 def number_of_teams(team_list)
-
+  team_list.split(", ").count
 end
 
 def return_australia(team_list)
-
+  # team_list.split(", ")[6]
+  # team_list.scan("Australia").join("")
+  team_list.scan("Australia").first
 end
 
 def starts_with_C(team_list)
-
+  # team_list.split(", ").select { |country| country[0] == "C"}
+  # team_list.split(", ").select { |c| c.include? ('C') }
+  team_list.split(", ").select { |c| c.chars.first ('C') }
 end
 
 def block_string_to_single_line(long_string)
-
+  # long_string.split(", "),join("").gsub("/n", " ").strip
+  long_string.gsub(/,/, "").gsub(/\n/, " ").strip
 end
 
 def capitalize_every_third_word(long_string)
-
+#   third = long_string.chomp.gsub("\n", " ").downcase.split(", ")
+#   stuff = []
+#   third.each_with_index do |x, i|
+#     if i == 0 || i % 3 == 0
+#       stuff[i] = x.capitalize!
+#     end
+#     stuff[i] = x
+#   end
+#   return stuff = stuff.join(" ")
+  block_string_to_single_line(long_string.downcase).split.each_slice(3)
+  .flat_map{|a,b,c| [a.capitalize, b, c]}.join(' ')
 end
 
 check("goal method", goal(goal_message) == "GOOOOOOOAL!!!!GOOOOOOOAL!!!!")
